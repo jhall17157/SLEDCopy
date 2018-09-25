db762db56640ba801eaa765a85ec50ddd7f49fdb
Grant Von Haden <grantvonhaden@gmail.com>
2018-09-25 13:04:00 -0500

    Added Mockups for viewing purposes

---
+++ CLS-SLE/Controllers/MockupController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CLS_SLE.Controllers
+{
+    public class MockupController : Controller
+    {
+        public ActionResult FacultyLandingPage()
+        {
+            return View();
+        }
+
+        public ActionResult TSAAssessment()
+        {
+            return View();
+        }
+
+        public ActionResult CLSAssessment()
+        {
+            return View();
+        }
+
+        public ActionResult AdministratorLandingPage()
+        {
+            return View();
+        }
+    }
+}

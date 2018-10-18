848d9b8b4a07d1d09c7d9fc126d56c66d21c9ab7
Mitchell Fleming <MFleming10@my.wctc.edu>
2018-10-18 10:03:37 -0500

    Added security hash

--- CLS-SLE/Controllers/HomeController.cs
+++ CLS-SLE/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CLS_SLE.Models;
+using CLS_SLE.Security;
+using System.Web.Security;
+using System.Net.Mail;
 
 namespace CLS_SLE.Controllers
 {

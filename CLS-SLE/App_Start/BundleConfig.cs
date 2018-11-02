cb5a4ea6dc2d60ed20b290f66a9cb14d2a66eeee
bhanson5 <bhanson5@my.wctc.edu>
2018-11-02 15:55:43 -0500

    Add WCTC Logo to fixed navbar

--- CLS-SLE/App_Start/BundleConfig.cs
+++ CLS-SLE/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CLS_SLE
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }

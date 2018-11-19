b4acd52e2b90c5519defa7cf165028e9bb3ce1ba
Mitchell Fleming <MFleming10@my.wctc.edu>
2018-11-19 16:23:02 -0600

    fontawesome css cleanup, manually added to bundleconfig.cs. Downward icons added to assessment page

--- CLS-SLE/App_Start/BundleConfig.cs
+++ CLS-SLE/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace CLS_SLE
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }

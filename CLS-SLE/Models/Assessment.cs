72595e989056ba245df5c7f6a525edf909d50485
bhanson5 <bhanson5@my.wctc.edu>
2018-11-28 22:09:20 -0600

    Update models

--- CLS-SLE/Models/Assessment.cs
+++ CLS-SLE/Models/Assessment.cs
@@ -24,8 +24,11 @@ namespace CLS_SLE.Models
         public string Name { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
+        public Nullable<decimal> OutcomePassRate { get; set; }
+        public bool CalculateOutcomePassRate { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AssessmentRubric> AssessmentRubrics { get; set; }
+        public virtual AssessmentCategory AssessmentCategory { get; set; }
     }
 }

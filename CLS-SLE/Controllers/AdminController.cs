05a00814246c8067f5dd762598e9de312c36b44c
smcgovern2 <smcgovern2@my.wctc.edu>
2019-03-11 20:28:57 -0500

    Framework for Role Manager, Non-functional

--- CLS-SLE/Controllers/AdminController.cs
+++ CLS-SLE/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CLS_SLE.Models;
+using NLog;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,10 @@ namespace CLS_SLE.Controllers
 {
     public class AdminController : Controller
     {
+
+        
+        private SLE_TrackingEntities db = new SLE_TrackingEntities();
+        private Logger logger = LogManager.GetCurrentClassLogger();
         // GET: Admin
         public ActionResult Index()
         {
@@ -46,8 +53,52 @@ namespace CLS_SLE.Controllers
 
         public ActionResult UsersSecurity()
         {
-            return View();
+            try
+            {
+                var People = from user in db.Users
+                             join person in db.People on user.PersonID equals person.PersonID
+                             select new { FirstName = person.FirstName, LastName = person.LastName, PersonID = person.PersonID, IDNumber = person.IdNumber };
+                var UserRoles = from role in db.Roles
+                                join userRole in db.UserRoles on role.RoleID equals userRole.RoleID
+                                join user in db.Users on userRole.PersonID equals user.PersonID
+                                select new { PersonID = user.PersonID, RoleName = role.Name, RoleID = role.RoleID };
+
+                dynamic MyModel = new ExpandoObject();
+                var PersonList = People.ToList();
+                var UserRoleList = UserRoles.ToList();
+                var UserSecurityList = new List<UserSecurity>();
+                foreach(var person in PersonList)
+                {
+                    var personRoles = new List<Role>();
+                    foreach (var userRole in UserRoleList)
+                    {
+                        if (userRole.PersonID.Equals(person.PersonID))
+                        {
+                            Role role = new Role();
+                            role.RoleID = userRole.RoleID;
+                            role.Name = userRole.RoleName;
+                            personRoles.Add(role);
+                        }
+                    }
+                    UserSecurityList.Add(new UserSecurity(person.PersonID, person.IDNumber, person.FirstName, person.LastName, personRoles));
+                }
+
+                MyModel.UserSecurityList = UserSecurityList;
+
+                return View(MyModel);
+            }
+            catch
+            {
+                logger.Error("Error fetching user List");
+                return Exceptions();
+            }
         }
+
+        private ActionResult Exceptions()
+        {
+            return RedirectToAction(actionName: "AdminDashboard", controllerName: "Admin");
+        }
+
 
     }
 }
---
+++ CLS-SLE/Models/UserSecurity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.Models
+{
+    public class UserSecurity
+    {
+        public UserSecurity(int personID, string iDNumber, string firstName, string lastName, List<Role> roles)
+        {
+            PersonID = personID;
+            IDNumber = iDNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            Roles = roles;
+        }
+
+        public int PersonID { get; set; }
+        public String IDNumber { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public List<Role> Roles { get; set; }
+
+    }
+}

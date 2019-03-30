3c4bdb3b0c05f6fe549e7009a5426d179204a9b5
Eric Riedel <eriedel1@my.wctc.edu>
2019-03-30 16:26:11 -0500

    ??

--- CLS-SLE/Models/SLE_TrackingModel.Designer.cs
+++ CLS-SLE/Models/SLE_TrackingModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Sean\source\repos\CLS-SLE\CLS-SLE\Models\SLE_TrackingModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Documents\CLS-SLE-NEW\CLS-SLE\Models\SLE_TrackingModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

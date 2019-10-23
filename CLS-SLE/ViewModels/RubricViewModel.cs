bc7e7737fbd3d7e2a0680b9ee1bcb9974a85d579
Aaron Saunders <ASaunders2@my.wctc.edu>
2019-10-22 20:37:00 -0500

    Created RubricViewModel and started converting AddRubric and InsertNewRubric inside of the RubricController

---
+++ CLS-SLE/ViewModels/RubricViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class RubricViewModel
+    {
+        public AssessmentRubric AssessmentRubric { get; set; }
+        public RubricAssessment RubricAssesssment { get; set; }
+    }
+}

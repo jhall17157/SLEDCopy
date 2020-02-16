efc5e9c1b16847b7d057b711820fce41223ceb1a
Lucas Wolfs <lwolfs@my.wctc.edu>
2020-02-15 19:23:58 -0600

    Partial implementation of mapping controller. Able to select programs from dropdown list and pass them to controller. Still need to retrieve data to populate mapping list.

---
+++ CLS-SLE/Controllers/AdminMappingController.cs
@@ -0,0 +1,67 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CLS_SLE.ViewModels;
+using System.Diagnostics;
+
+namespace CLS_SLE.Controllers
+{
+
+    //New controller for mapping page
+    public class AdminMappingController : Controller
+    {
+        private SLE_TrackingEntities db = new SLE_TrackingEntities();
+
+
+        // GET: AdminMapping home page with list of Programs sorted by id
+        public ActionResult Index()
+        {
+            MappingViewModel mappingViewModel = new MappingViewModel();
+            var availablePrograms = db.Programs;
+            mappingViewModel.AvailablePrograms = new SelectList(availablePrograms, "ProgramID", "Name");
+            
+            return View(mappingViewModel);
+        }
+
+
+        //public ActionResult ViewMapping(MappingViewModel mappingViewModel)
+        //{
+        //    Debug.WriteLine(mappingViewModel.ProgramID + "program id");
+        //    Debug.WriteLine(mappingViewModel.Program.Name);
+
+
+        //    return View(mappingViewModel);
+        //}
+
+
+
+
+
+        ////
+        //[HttpPost]
+        //public ActionResult GetAssessmentsForProgram(MappingViewModel mappingViewModel)
+        //{
+        //    Debug.WriteLine(mappingViewModel.ProgramID);
+        //    Debug.WriteLine(db.Programs.Where(p => p.ProgramID == mappingViewModel.ProgramID).FirstOrDefault().Name);
+
+        //    if(ModelState.IsValid)
+        //    {
+        //        //mappingViewModel.Programs = db.Programs.OrderBy(p => p.ProgramID);
+        //        mappingViewModel.Program = db.Programs.Where(p => p.ProgramID == mappingViewModel.ProgramID).FirstOrDefault();
+        //        mappingViewModel.Assessments = db.Assessments.Where(a => a.ProgramID == mappingViewModel.ProgramID).ToList();
+  
+        //    }
+        //    else
+        //    {
+                
+        //        return RedirectToAction("Index", "AdminMapping");
+        //    }
+        //    return RedirectToAction("ViewMapping", "AdminMapping");
+        //}
+
+        
+    }
+}
---
+++ CLS-SLE/ViewModels/MappingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CLS_SLE.Models;
+
+namespace CLS_SLE.ViewModels
+{
+    //New view model for mappings
+    public class MappingViewModel
+    {
+        public SelectList AvailablePrograms { get; set; }
+        public int? SelectedProgram { get; set; }
+        //public SelectList Programs { get; set; }
+        //public int? ProgramID { get; set; }
+        public Program Program { get; set; }
+        public IEnumerable<Assessment> Assessments { get; set; }
+
+
+    }
+}

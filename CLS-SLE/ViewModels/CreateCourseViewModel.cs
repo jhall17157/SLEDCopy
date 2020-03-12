3411261f2983541879ca9a2f74d38ffb500c9355
Samuel Tom <stom@my.wctc.edu>
2020-03-12 09:14:02 -0500

    Courses Commit for sprint 3

---
+++ CLS-SLE/ViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class CreateCourseViewModel
+    {
+        public Course Course { get; set; }
+        public String DepartmentSelection { get; set; }
+    }
+}

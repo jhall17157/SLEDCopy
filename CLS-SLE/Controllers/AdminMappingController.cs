a075eae9a4d1b65444e7ee5f968654541f095918
Lucas Wolfs <lwolfs@my.wctc.edu>
2020-04-03 16:34:46 -0500

    Added populating of selectlist item on page load for mapping controller

--- CLS-SLE/Controllers/AdminMappingController.cs
+++ CLS-SLE/Controllers/AdminMappingController.cs
@@ -26,6 +26,8 @@ namespace CLS_SLE.Controllers
                                         where !c.CourseName.Contains("Folio180")
                                         select new SelectListItem { Text = c.Number + " " + c.CourseName, Value = c.CourseID.ToString() })
                                             .Distinct().ToList();
+            mappingViewModel.Rubrics = (from r in db.AssessmentRubrics
+                                        select new SelectListItem { Text = r.Name, Value = r.RubricID.ToString() }).Distinct().ToList();
             return View(mappingViewModel);
         }
 

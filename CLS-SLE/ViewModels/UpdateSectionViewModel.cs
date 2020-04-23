e724a9f5adf277af14a25e57b96ab54c55cbb734
klanguyen <nnguyen1@my.wctc.edu>
2020-04-23 18:13:31 -0500

    Edit page works but database does not change

--- CLS-SLE/ViewModels/UpdateSectionViewModel.cs
+++ CLS-SLE/ViewModels/UpdateSectionViewModel.cs
@@ -9,8 +9,16 @@ namespace CLS_SLE.ViewModels
     public class UpdateSectionViewModel
     {
         public Section Section { get; set; }
+
         public IEnumerable<String> SemesterNames { get; set; }
         public String SemesterSelection { get; set; }
+
+        public IEnumerable<String> CourseNames { get; set; }
+        public String CourseSelection { get; set; }
+
+        public IEnumerable<String> LeadInstructorList { get; set; }
+        public String LeadInstructorSelection { get; set; }
+
         public bool IsCancelled { get; set; }
     }
 }

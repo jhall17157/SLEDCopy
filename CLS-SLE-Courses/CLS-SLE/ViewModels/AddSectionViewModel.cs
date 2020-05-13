2c412f2a0a822ddef2798decb6e4ea291c08cddd
SamTRexTom <samtrextom@gmail.com>
2020-05-13 12:40:38 -0500

    More updates 5/13/2020

--- CLS-SLE-Courses/CLS-SLE/ViewModels/AddSectionViewModel.cs
+++ CLS-SLE-Courses/CLS-SLE/ViewModels/AddSectionViewModel.cs
@@ -10,10 +10,8 @@ namespace CLS_SLE.ViewModels
     public class AddSectionViewModel
     {
         public Section Section { get; set; }
-
         public List<String> SemesterList { get; set; }
         public String SemesterSelection { get; set; }
-
         public List<String> LeadInstructorList { get; set; }
         public String LeadInstructorSelection { get; set; }
     }

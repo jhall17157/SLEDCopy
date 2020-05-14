99ba068337053563cadb968625999fa996d55d07
Tyler Mastropolo <54603892+tmastropolo@users.noreply.github.com>
2020-05-14 11:20:27 -0500

    Tyler's changes
    Added alerts and departments to program page

---
+++ ViewModels/RubricViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class RubricViewModel
+    {
+        public AssessmentRubric AssessmentRubric { get; set; }
+        public RubricAssessment RubricAssesssment { get; set; }
+
+        public int AssessmentID { get; set; }
+        public List<string> AssessmentList { get; set; }
+    }
+}

584381780d526fb954de05949c8b566b6e9e76ea
cschackel <cschackel@my.wctc.edu>
2020-09-20 17:39:07 -0500

    Changed JQuery UI scripts from CDN to Local Files

--- CLS-SLE/App_Start/BundleConfig2.cs
+++ CLS-SLE/App_Start/BundleConfig2.cs
@@ -22,9 +22,16 @@ namespace CLS_SLE
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
+                 "~/Scripts/jquery-ui*"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jqueryUI").Include(
+                      "~/Content/jquery-ui*"));
+
         }
     }
 }

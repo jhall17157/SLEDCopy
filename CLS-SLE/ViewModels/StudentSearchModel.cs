8bff0dd57afaa7a0f690cdd3bb0e1d6028b63b3b
cschackel <cschackel@my.wctc.edu>
2020-09-20 17:08:23 -0500

    AutoSuggest Feature Reenabled. Jquery UI Added and JQuery UI Styling added. CSS Modifyed to show AutoComplete Suggestions on Top

---
+++ CLS-SLE/ViewModels/StudentSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class StudentSearchModel
+    {
+        public string idNumber { get; set; }
+        public string lastName { get; set; }
+
+        public string firstName { get; set; }
+        
+    }
+}

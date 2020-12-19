8a0854b66f2eb3f49b90b094865e72543e260d92
Nicki Kowalchuk <nkowalchuk@wctc.edu>
2020-12-19 15:01:00 -0600

    fixed merge issue

---
+++ CLS-SLE/ViewModels/SchoolsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class SchoolsViewModel
+    {
+        public List<School> activeSchools { get; set; }
+        public List<School> inActiveSchools { get; set; }
+        public string updatedMessage { get; set; }
+        public string alertMessage { get; set; }
+    }
+}

844ca5013781d8b89c04123b9a948785d8b8f7c3
Neal Howard <nhoward72@yahoo.com>
2021-03-04 21:24:49 -0600

    Git Erased my progress

---
+++ CLS-SLE/ViewModels/ScoreSetViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CLS_SLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLS_SLE.ViewModels
+{
+    public class ScoreSetViewModel
+    {
+
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public List<ScoreSet> ScoreSets { get; set; }
+    }
+}
